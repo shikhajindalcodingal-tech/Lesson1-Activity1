894ed2ac1131d17c19a5ede67c00141267eeeb49
Shikha.condingal <shikhajindal.codingal@gmail.com>
2025-09-10 21:30:28 +0530

    Strings

--- String-operations.py
+++ String-operations.py
@@ -1,1 +1,10 @@
 print("String")
+text = str(input("Enter a string: "))
+
+# Reverse String
+# using step value as -1 to iterate in reverse
+revText = text[::-1]
+text = revText
+
+print("Reverse of Given String is:")
+print(text)
--- String_concatenation.py
+++ String_concatenation.py
@@ -1,1 +1,11 @@
 print("Concatenate the strings")
+print("Enter the First String: ")
+strOne = input()
+
+print("Enter the Second String: ")
+strTwo = input()
+
+# Concatenate the two strings
+# Using Addition Operator
+strThree = strOne + strTwo
+print("\nConcatenated String: ", strThree)
